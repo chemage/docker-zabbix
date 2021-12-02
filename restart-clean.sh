d6a9e1ddea6cb2622ef9cf4f115fde7ba1bef63e
Marcel Gerber <9885015+chemage@users.noreply.github.com>
2021-12-02 21:57:14 +0100

    add rm zabbixnet

--- restart-clean.sh
+++ restart-clean.sh
@@ -3,6 +3,7 @@
 # give docker-compose file as param
 docker-compose -f $1 stop
 docker-compose -f $1 rm
+docker network rm zabbixnet
 rm -rf zabbix* mysql
 ls -lAh --color
 docker-compose -f $1 up -d
