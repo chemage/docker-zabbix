8fa20a4c49acb210b810959bc91b59c0ef435e2b
Marcel Gerber <9885015+chemage@users.noreply.github.com>
2021-12-02 21:53:46 +0100

    add restart-clean.sh

---
+++ restart-clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Stop containers, remove them and start afresh
+# give docker-compose file as param
+docker-compose -f $1 stop
+docker-compose -f $1 rm
+rm -rf zabbix* mysql
+ls -lAh --color
+docker-compose -f $1 up -d
